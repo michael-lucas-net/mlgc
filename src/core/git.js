f3bf2f55a246e9328eaadad8a88dde80f5996f8d
Michael Lucas <michael-lucas-net@users.noreply.github.com>
2024-11-22 16:41:07 +0100

    get current mainbranchname

--- src/core/git.js
+++ src/core/git.js
@@ -1,4 +1,5 @@
 const settings = require("../../config/settings");
+const { execSync } = require("child_process");
 
 function getIgnoredFiles() {
   return settings["ignored-files"]
@@ -9,8 +10,23 @@ function getIgnoredFiles() {
 function gitCommand(branchOrCommit) {
   const baseCommand = "git add -N . && git diff --name-only --diff-filter=d ";
   return branchOrCommit === "branch"
-    ? `${baseCommand}origin/${settings["main-branch"]} ${getIgnoredFiles()}`
+    ? `${baseCommand}origin/${getMainBranch()} ${getIgnoredFiles()}`
     : `${baseCommand} ${getIgnoredFiles()}`;
+}
+
+function getMainBranch() {
+  try {
+    // Führe den Git-Befehl aus
+    const output = execSync("git symbolic-ref refs/remotes/origin/HEAD", {
+      encoding: "utf-8",
+    });
+    // Extrahiere den Branch-Namen aus dem Pfad
+    return output.trim().split("/").pop();
+  } catch (error) {
+    console.error("Error detecting main branch:", error.message);
+    // Fallback auf "master", falls der Befehl fehlschlägt
+    return "master";
+  }
 }
 
 module.exports = { gitCommand };
